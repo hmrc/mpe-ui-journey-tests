e52715468dd0f1afbc069634153b3219c27e0c35
prode13 <190367850+prode13@users.noreply.github.com>
2025-10-17 10:01:19 +0100

    Upgraded the test runner and cucumber dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,10 +3,10 @@ import sbt.*
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "ui-test-runner" % "0.49.0",
+    "uk.gov.hmrc"       %% "ui-test-runner" % "0.50.0",
     "org.scalatestplus" %% "selenium-4-21"   % "3.2.19.0",
     "io.cucumber"       %% "cucumber-scala" % "8.31.0",
-    "io.cucumber"        % "cucumber-junit" % "7.27.0",
+    "io.cucumber"        % "cucumber-junit" % "7.30.0",
     "org.playframework" %% "play-test"      % "3.0.8" exclude("ch.qos.logback", "logback-classic")
   )
 
